fc34d5f8d1319db5f70cc67110fcd86ea54f3745
thi <vannguyen141290@gmail.com>
2021-09-30 09:46:14 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+
+console.log('hello')
+console.log(process.env.USERNAME)
+console.log(process.env.FOO)
+
+const express = require('express')
+
+const server = express()
+
+server.use(express.json())
+
+const port = process.env.PORT || 5000
+
+server.get('/hello', (req, res) => {
+    res.send('<h1>Hello from VAN</h1>')
+})
+
+server.listen(port, () => {
+    console.log(`\n*** Server listening on port ${port} ***\n`)
+})

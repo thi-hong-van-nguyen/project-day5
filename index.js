9280ee9165096f78cc2ebb41d994afb69bd9581e
thi <vannguyen141290@gmail.com>
2021-09-30 10:46:46 -0700

    react

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
 require('dotenv').config()
-
-console.log('hello')
-console.log(process.env.USERNAME)
-console.log(process.env.FOO)
+const path = require('path')
 
 const express = require('express')
 
 const server = express()
 
 server.use(express.json())
-
-const port = process.env.PORT || 5000
-
-server.get('/', (req, res) => {
-    res.send('<h1>Hello ffgg</h1>')
-})
+server.use(express.static(
+    path.join(__dirname, 'client/build')
+))
 
 
 server.get('/hello', (req, res) => {
     res.send('<h1>Hello from VAN</h1>')
 })
+
+server.get('*', (req, res) => {
+    res.sendFile(
+        path.join(__dirname, 'client/build', 'index.html')
+    )
+})
+
+const port = process.env.PORT || 5000
 
 server.listen(port, () => {
     console.log(`\n*** Server listening on port ${port} ***\n`)
